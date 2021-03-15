c56e6f5215f7226a6cd18df8f8e6f65e68c7ceb9
sunset-summer <alex1123321@gmail.com>
2021-03-15 05:57:30 +0200

    fead: add solution to task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (matrix === undefined) {
+        return [];
+    }
+    let fMatrix = [];
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 == 0) {
+            for (let k = 0; k < matrix[i].length; k++) {
+                fMatrix.unshift(matrix[i][k]);
+            } 
+        } else {
+            let temp = matrix[i].reverse();
+            for (let j = 0; j < temp.length; j++) {
+                fMatrix.unshift(temp[j]);
+            } 
+        }
+    }
+revertedMatrix = fMatrix.reverse();
+return revertedMatrix;
 }
